f4e8fe8179d50233fe18f2b5d904941f1fe3f3fe
3A532055 <ylhc214044@gmail.com>
2018-10-31 13:43:45 +0800

    CH5-3..設定選擇性路由參數(科目)

--- routes/web.php
+++ routes/web.php
@@ -23,6 +23,6 @@ Route::get('student/{student_no}/score', function($student_no){
     return "學號：".$student_no." 的所有成績" ;
 });
 
-Route::get('student/{student_no}/score/{subject}', function($student_no,$subject){
-    return "學號：".$student_no." 的 ".$subject." 所有成績" ;
+Route::get('student/{student_no}/score/{subject?}', function($student_no,$subject = null){
+    return "學號：".$student_no." 的 ".((is_null($subject))?" 所有科目 ":$subject). "成績" ;
 });
